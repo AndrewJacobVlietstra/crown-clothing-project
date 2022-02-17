e5378d25ffbd070379d40408db57deaeca35085a
Andrew Vlietstra <vlietstra123@hotmail.com>
2022-02-17 15:15:36 -0500

    Created HomePage component and enabled node-sass package

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import HomePage from "./components/HomePage/HomePage";
 
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
 }
 
 export default App;
