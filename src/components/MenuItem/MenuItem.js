099d823563001205d96091d24314087f28d9bc78
Andrew Vlietstra <vlietstra123@hotmail.com>
2022-02-18 19:43:23 -0500

    imported useNavigate to navigate to the correct shop page when a menuItem is clicked

--- src/components/MenuItem/MenuItem.js
+++ src/components/MenuItem/MenuItem.js
@@ -1,9 +1,12 @@
 import React from "react";
 import "./MenuItem.styles.scss";
+import { useNavigate } from "react-router-dom";
 
-const MenuItem = ({ title, imageUrl, size }) => {
+const MenuItem = ({ title, imageUrl, size, linkUrl }) => {
+  let navigate = useNavigate();
+
   return (
-    <div className={size ? `${size} menu-item` : 'menu-item'}>
+    <div className={size ? `${size} menu-item` : 'menu-item'} onClick={() => navigate(`${linkUrl}`)}>
       <div className="background-image" style={{ backgroundImage: `url(${imageUrl})` }} />
       <div className="content">
         <h1 className="title">{title}</h1>
