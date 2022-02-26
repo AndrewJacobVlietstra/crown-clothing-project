c06d9f4d125323979199ba68799bd4c5144c6a32
Andrew Vlietstra <vlietstra123@hotmail.com>
2022-02-25 19:25:33 -0500

    Added reduce methods to calculate total number of items in cart as well as total price of the items in the cart

--- src/components/CartDropdown/CartDropdown.js
+++ src/components/CartDropdown/CartDropdown.js
@@ -5,8 +5,13 @@ import CustomButton from "../CustomButton/CustomButton";
 import { connect } from "react-redux";
 
 const CartDropdown = ({ cartItems }) => {
+  const totalCartPrice = cartItems.reduce((previousValue, currentValue) => {
+    return previousValue + (currentValue.price * currentValue.quantity);
+  }, 0);
+
   return (
     <div className="cart-dropdown">
+      {totalCartPrice ? <span className="total">{`Total Cost $${totalCartPrice}`}</span> : null}
       <div className="cart-items">
         {cartItems.map( cartItem => <CartItem key={cartItem.id} item={cartItem} />)}
       </div>
--- src/components/CartIcon/CartIcon.js
+++ src/components/CartIcon/CartIcon.js
@@ -4,17 +4,29 @@ import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 import { connect } from 'react-redux';
 import { toggleCartDropdownAction } from '../../redux/cart/cart.actions';
 
-const CartIcon = ({ toggleCartDropdown }) => {
+const CartIcon = ({ cartItems, toggleCartDropdown }) => {
+  // Use a array reduce method on all cart items to calculate the total quantity of items in the cart
+  // The total amount will be recalculated any time the cartItems change
+  const totalCartItems = cartItems.reduce((previousValue, currentValue) => {
+    return previousValue + currentValue.quantity;
+  }, 0);
+
+  console.log(totalCartItems);
+
   return (
     <div className='cart-icon' onClick={toggleCartDropdown}>
       <ShoppingIcon className='shopping-icon' />
-      <span className='item-count'>0</span>
+      <span className='item-count'>{totalCartItems}</span>
     </div>
   )
 }
+
+const mapStateToProps = ({ cart }) => ({
+  cartItems: cart.cartItems
+});
 
 const mapDispatchToProps = dispatch => ({
   toggleCartDropdown: () => dispatch(toggleCartDropdownAction())
 });
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
