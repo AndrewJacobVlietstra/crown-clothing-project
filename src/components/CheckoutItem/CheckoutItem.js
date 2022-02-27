6fe1fad9469ad6381e4757e9644d3b87096e10fc
Andrew Vlietstra <vlietstra123@hotmail.com>
2022-02-27 15:40:16 -0500

    Converted css of checkout page back to width percentages to have columns be always lined up even as window size decreases

--- src/components/CheckoutItem/CheckoutItem.js
+++ src/components/CheckoutItem/CheckoutItem.js
@@ -1,18 +1,41 @@
-import React from 'react';
-import './CheckoutItem.styles.scss';
+import React from "react";
+import "./CheckoutItem.styles.scss";
+import { connect } from "react-redux";
+import {
+  removeItemFromCartAction,
+  decrementItemQuantityAction,
+  addItemToCartAction,
+} from "../../redux/cart/cart.actions";
 
-const CheckoutItem = ({ item }) => {
+const CheckoutItem = ({ item, addItem, decreaseItem, removeItem }) => {
   return (
-    <div className='checkout-item'>
-      <div className='image-container'>
-        <img src={item.imageUrl} alt='item' />
+    <div className="checkout-item">
+      <div className="image-container">
+        <img src={item.imageUrl} alt="item" />
       </div>
-      <span className='name'>{item.name}</span>
-      <span className='quantity'> <span>&#10094;</span> {item.quantity} <span>&#10095;</span> </span>
-      <span className='price'>{`$${item.price}`}</span>
-      <div className='remove-button'>&#10005;</div>
+      <span className="name">{item.name}</span>
+      <span className="quantity">
+        {" "}
+        <span className="quantity-button" onClick={() => decreaseItem(item)}>
+          &#10094;
+        </span>{" "}
+        {item.quantity}{" "}
+        <span className="quantity-button" onClick={() => addItem(item)}>
+          &#10095;
+        </span>{" "}
+      </span>
+      <span className="price">{`$${item.price * item.quantity}`}</span>
+      <div className="remove-button" onClick={() => removeItem(item)}>
+        &#10005;
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default CheckoutItem;
+const mapDispatchToProps = (dispatch) => ({
+  removeItem: (item) => dispatch(removeItemFromCartAction(item)),
+  addItem: (item) => dispatch(addItemToCartAction(item)),
+  decreaseItem: (item) => dispatch(decrementItemQuantityAction(item)),
+});
+
+export default connect(null, mapDispatchToProps)(CheckoutItem);
--- src/pages/CheckoutPage/CheckoutPage.js
+++ src/pages/CheckoutPage/CheckoutPage.js
@@ -1,41 +1,52 @@
-import React from 'react';
-import './CheckoutPage.styles.scss';
-import { connect } from 'react-redux'
-import { cartItemsSelector, cartTotalPriceSelector } from "../../redux/cart/cart.selectors";
-import CheckoutItem from '../../components/CheckoutItem/CheckoutItem';
+import React from "react";
+import "./CheckoutPage.styles.scss";
+import { connect } from "react-redux";
+import {
+  cartItemsSelector,
+  cartTotalPriceSelector,
+} from "../../redux/cart/cart.selectors";
+import CheckoutItem from "../../components/CheckoutItem/CheckoutItem";
 
-const CheckoutPage = ({cartItems, totalCartPrice}) => {
+const CheckoutPage = ({ cartItems, totalCartPrice }) => {
   return (
-    <div className='checkout-page'>
-      <div className='checkout-header'>
-        <div className='header-block'>
-          <span className='product-header'>Product</span>
+    <div className="checkout-page">
+      <div className="checkout-header">
+        <div className="header-block">
+          <span className="product-header">Product</span>
         </div>
-        <div className='header-block'>
-          <span className='description-header'>Description</span>
+        <div className="header-block">
+          <span className="description-header">Description</span>
         </div>
-        <div className='header-block'>
-          <span className='quantity-header'>Quantity</span>
+        <div className="header-block">
+          <span className="quantity-header">Quantity</span>
         </div>
-        <div className='header-block'>
-          <span className='price-header'>Price</span>
+        <div className="header-block">
+          <span className="price-header">Price</span>
         </div>
-        <div className='header-block'>
-          <span className='remove-header'>Remove</span>
+        <div className="header-block">
+          <span className="remove-header">Remove</span>
         </div>
       </div>
 
-      {cartItems.map(item => <CheckoutItem key={item.id} item={item} />)}
-      <div className='total'>
+      {cartItems.map((item) => (
+        <CheckoutItem key={item.id} item={item} />
+      ))}
+      {cartItems.length ? null : (
+        <p className="empty-message">
+          There are currently no items in your cart, head to the Shop to add
+          some!
+        </p>
+      )}
+      <div className="total">
         <span>{`TOTAL: $${totalCartPrice}`}</span>
       </div>
     </div>
-  )
-}
+  );
+};
 
-const mapStateToProps = state => ({
+const mapStateToProps = (state) => ({
   cartItems: cartItemsSelector(state),
-  totalCartPrice: cartTotalPriceSelector(state)
+  totalCartPrice: cartTotalPriceSelector(state),
 });
 
 export default connect(mapStateToProps)(CheckoutPage);
