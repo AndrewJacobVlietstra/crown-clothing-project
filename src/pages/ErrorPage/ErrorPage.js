1f2cdca2c54a57c68d099e9b42e96b02bcb6a30c
Andrew Vlietstra <vlietstra123@hotmail.com>
2022-03-01 20:35:08 -0500

    Styled and changed up error page

--- src/pages/ErrorPage/ErrorPage.js
+++ src/pages/ErrorPage/ErrorPage.js
@@ -4,8 +4,10 @@ import './ErrorPage.styles.scss'
 const ErrorPage = () => {
   return (
     <div className='error-page'>
-      <h1>ERROR</h1>
-      <p>This Page does not exist, please go back!</p>
+      <h1 className='error-title'>404 ERROR</h1>
+      <h2 className='error-message'>Oops! Page not found!</h2>
+      <p className='error-sub-message'>Sorry the page you're looking for doesn't exist!</p>
+      <button className='back-button' onClick={() => window.history.back()}>Go Back</button>
     </div>
   )
 }
