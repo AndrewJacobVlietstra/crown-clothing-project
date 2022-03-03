447f5231ec3b1ad446541b4a8cdd4a49bc05ac26
Andrew Vlietstra <vlietstra123@hotmail.com>
2022-03-03 10:20:55 -0500

    Added contact page and made it responsive

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -3,7 +3,17 @@ import './ContactForm.styles.scss';
 
 const ContactForm = () => {
   return (
-    <div>ContactForm</div>
+    <div className='contact-form-container'>
+      <form className='contact-form'>
+        <h2 className='form-title'>Send us a message 🚀</h2>
+        <input type='text' placeholder='Full name*' className='name-input' required></input>
+        <input type='email' placeholder='Email*' className='email-input' required></input>
+        <input type='text' placeholder='Subject' className='subject-input'></input>
+        <label className='big-message-label' htmlFor='big-message'>Tell us more about yourself *</label>
+        <textarea rows='7' id='big-message' required></textarea>
+        <button type='submit' className='contact-form-submit'>Send Message</button>
+      </form>
+    </div>
   )
 }
 
--- src/components/ContactMessage/ContactMessage.js
+++ src/components/ContactMessage/ContactMessage.js
@@ -1,10 +1,25 @@
-import React from 'react';
-import './ContactMessage.styles.scss';
+import React from "react";
+import "./ContactMessage.styles.scss";
+import { ReactComponent as Envelope } from "../../assets/envelope-solid.svg";
 
 const ContactMessage = () => {
   return (
-    <div>ContactMessage</div>
-  )
-}
+    <div className="contact-message-container">
+      <h1 className="contact-message-title">
+        Let's chat.
+        <br />
+        Tell me about yourself.
+      </h1>
+      <p className="contact-sub-message">Let's build a project together 💪</p>
+      <div className="contact-me-container">
+        <Envelope className="contact-me-envelope" />
+        <p className="contact-me">
+          Mail me at <br />
+          <a className="contact-me-link">example@gmail.com</a>
+        </p>
+      </div>
+    </div>
+  );
+};
 
 export default ContactMessage;
