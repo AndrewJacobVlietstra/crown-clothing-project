d5dd301a084aeadffa0419efe12374d21bfb91b6
Andrew Vlietstra <vlietstra123@hotmail.com>
2022-03-09 11:18:31 -0500

    Uploading firebase data

---
+++ src/firebase/firebase.data.js
@@ -0,0 +1,4 @@
+export const firebaseApiKey = 'AIzaSyCZjXZR7IXX7gF7rkvQJdpkYXeeRwqaWU4';
+export const firebaseAppID = '1:925857362146:web:6541d0c6490580c185eebb';
+export const firebaseMessagingSenderID = '925857362146';
+export const firebaseMeasurementID = 'G-9K8ZEX38MD';
